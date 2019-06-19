8263f8f3660099c1717d0a3a990d59a8b6626686
aiwaverse <aiwavision@protonmail.com>
2019-06-19 18:46:25 -0300

    small refiniments

--- ordered.cpp
+++ ordered.cpp
@@ -1,12 +1,13 @@
 #include "ordered.hpp"
 #include <iostream>
+#include <stdexcept>
 namespace ordered {
 User_data Users::find(size_t n) {
-    return users_info.at(n);
+    return users_info.at(n-1);
 }
 void Users::insert(size_t user, unsigned movie_id, double rating) {
     auto new_element{std::make_pair(movie_id, rating)};
-    users_info[user].movie_ratings.push_back(new_element);
+    users_info.at(user-1).movie_ratings.push_back(new_element);
 }
 std::ostream& operator<<(std::ostream& os, const User_data& rhs){
     for(auto a:rhs.movie_ratings){
--- ordered.hpp
+++ ordered.hpp
@@ -12,7 +12,7 @@ struct User_data {
 class Users {
    private:
     //each array element models one user, and the vector inside models all the movies rated by this user
-    std::array<User_data, 200000> users_info{};
+    std::array<User_data, 138493> users_info{};
    public:
     User_data find(size_t user);
     void insert(size_t user, unsigned movie_id, double rating);
